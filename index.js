c16a2ee7411228d4f1a54cc2b4b333807369caf1
flamarionpinto <flamarionpinto@sou.faccat.br>
2021-10-02 18:16:08 -0300

    Resgatando o nome enviado em chat e mostrando na tela provisóriamente

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/chat", (req, res) => {
-  res.send(req.query.name);
+  res.render("chat", {
+    name: req.query.name
+  })
 });
 
 io.on("connection", (socket) => {
