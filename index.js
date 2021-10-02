af2484d9711097f5e1398b22492c3b6d70fe019d
flamarionpinto <flamarionpinto@sou.faccat.br>
2021-10-02 18:06:50 -0300

    enviando o valor de nome para o backend

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/chat", (req, res) => {
-  // res.send('working')
-  res.render("chat");
-})
+  res.send(req.query.name);
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
