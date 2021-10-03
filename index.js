defb7bb30262c4dd3fd907fa1f0083f52893a8ac
flamarionpinto <flamarionpinto@sou.faccat.br>
2021-10-02 21:07:56 -0300

    Envio da mensagem para o backend

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ app.get("/chat", (req, res) => {
   })
 });
 
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
+
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
